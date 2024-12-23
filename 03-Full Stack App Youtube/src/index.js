86f0e42a9a4f9b9e6728de03f673e1b748159f3b
SatyabratDeveloper <satyabrat.developer@gmail.com>
2024-12-23 18:50:20 +0530

    cookie-parser, cors and express added

--- 03-Full Stack App Youtube/src/index.js
+++ 03-Full Stack App Youtube/src/index.js
@@ -1,6 +1,14 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import app from "./app.js";
 
 dotenv.config({ path: "./env" });
 
-connectDB();
+const Port = process.env.PORT || 8000;
+
+connectDB()
+  .then(() => {
+    app.on("error", (error) => console.log("Express error:", error));
+    app.listen(Port, () => console.log(`Server is listing on port ${Port}`));
+  })
+  .catch((error) => console.log("MongoDB connection failed:", error));
